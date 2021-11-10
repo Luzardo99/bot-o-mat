eba8b603c69cdc9606ea53a6dcb2924f0a5fa1df
dl805387 <59711497+dl805387@users.noreply.github.com>
2021-11-10 14:32:14 -0500

    implemented form for bot name and type

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [name, setName] = useState("");
+    const [type, setType] = useState("");
+
+    return (
+        <div className="App">
+            <div onChange={(e) => {setType(e.target.value); console.log(e.target.value);}}>
+                <input type="radio" value="UNIPEDAL" name="gender"/> Unipedal
+                <input type="radio" value="BIPEDAL" name="gender"/> Bipedal
+                <input type="radio" value="QUADRUPEDAL" name="gender"/> Quadrupedal
+                <br></br>
+                <input type="radio" value="ARACHNID" name="gender"/> Arachnid
+                <input type="radio" value="RADIAL" name="gender"/> Radial
+                <input type="radio" value="AERONAUTICAL" name="gender"/> Aeronautical
+            </div>
+
+            <input value={name} onChange={(e) => {setName(e.target.value)}}/>
+            <button onClick={() => {console.log(type + " " + name)}}>Create Bot</button>
+        
+        </div>
+    );
 }
 
 export default App;
